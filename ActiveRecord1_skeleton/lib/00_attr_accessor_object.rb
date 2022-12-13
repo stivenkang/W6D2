c65308e5bab1708be7d34ac310e23628a2a24096
Stiven Kang <kdaegyung@gmail.com>
2022-12-13 16:48:29 -0500

    first commit

---
+++ ActiveRecord1_skeleton/lib/00_attr_accessor_object.rb
@@ -0,0 +1,12 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each{|name|
+      define_method(name) {
+        instance_variable_get("@#{name}")
+      }
+      define_method("#{name}=") {|val|
+        instance_variable_set("@#{name}", val)
+      }
+    }
+  end
+end
